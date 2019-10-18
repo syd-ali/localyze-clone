9dc7d6b4ad556a627f7c69067a60f6d990e6271f
syedaliimran070 <syedaliimran070@gmail.com>
2019-10-18 11:56:17 +0500

    Navigation un commented

--- App/Navigation/Navigator.js
+++ App/Navigation/Navigator.js
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import { createAppContainer } from 'react-navigation';
-// import { createBottomTabNavigator } from 'react-navigation-tabs';
-//
-// import {
-//   HomeScreen,
-//   VideoScreen
-// } from '../Screens';
-//
-// const MainTab = createBottomTabNavigator({
-//   Home: {screen: HomeScreen},
-//   Video: {screen: VideoScreen}
-// },{
-//   tabBarOptions: {
-//     initialRouteName: 'Video',
-//     activeTintColor: '#000',
-//     inactiveTintColor: '#ccc'
-//   }
-// });
-//
-// const AppNavigator = createAppContainer(MainTab);
-//
-// export default AppNavigator;
+ import React from 'react';
+ import { createAppContainer } from 'react-navigation';
+ import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+ import {
+   HomeScreen,
+   VideoScreen
+ } from '../Screens';
+
+ const MainTab = createBottomTabNavigator({
+   Home: {screen: HomeScreen},
+   Video: {screen: VideoScreen}
+ },{
+   tabBarOptions: {
+     initialRouteName: 'Video',
+     activeTintColor: '#000',
+     inactiveTintColor: '#ccc'
+   }
+ });
+
+ const AppNavigator = createAppContainer(MainTab);
+
+ export default AppNavigator;
